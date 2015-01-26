fed93afe56a49c9a62221ccc9a8ccebd67d274f8
Michael Broll <michael.r.broll@gmail.com>
2015-01-26 01:34:30 -0600

    updated docs

--- doc/html/search/all_1.js
+++ doc/html/search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['cursor',['Cursor',['../classFleaux_1_1Cursor.html',1,'Fleaux']]],
-  ['cursor',['Cursor',['../classFleaux_1_1Cursor.html#a4dcda4244b32b97d7251a64719a348b7',1,'Fleaux::Cursor']]]
+  ['cursor',['Cursor',['../classFleaux_1_1Cursor.html#aa2811c8ebadd1b019a1d1099eb401e3b',1,'Fleaux::Cursor::Cursor(Editor *ed)'],['../classFleaux_1_1Cursor.html#a643ea893489f5a963473dc79afd65bd2',1,'Fleaux::Cursor::Cursor(const ICursor &amp;curs)']]]
 ];
